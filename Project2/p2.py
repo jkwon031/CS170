74438b4f1890e3fd26c172b0cc1cffc8f3f456ed
Junyoung Kwon <jkwon031@bolt.cs.ucr.edu>
2019-12-10 12:59:25 -0800

    Finished leave-one-out

--- Project2/p2.py
+++ Project2/p2.py
@@ -1,66 +1,58 @@
 import os
 import math
 
-
-def nearestNeighborAlgorithm(data, curr_point, num_lines, curr_set):
-	nn = 0
-	shortestdist = float("inf")
-	#print("Curr point:", curr_point)
-
-	for i in range(num_lines - 1):
-	#	print("hi")
-		if curr_point != i:
-			dist = 0
-			for j in range(1, len(curr_set)):
-				dist = math.sqrt(dist + pow((data[curr_point][curr_set[j]] - data[i][curr_set[j]]), 2))
-				print(dist)
-
-			if dist < shortestdist:
-				nn = i
-				shortestdist = dist
-
-	return nn
-'''
-def search(data):
+def feature_search(data, num_features):
 	current_set_of_features = []
-	print(num_features)
 	for i in range(num_features):
-		print("On the", str(i + 1), "th level of the serach tree")
-		feature_adding = []
-		best_acc = 0
+		print("On the", str(i + 1), "th level of the search tree")
 		for k in range(num_features):
-			if k not in current_set_of_features:
-				print("Considering adding the", str(k + 1), "feature")
-				accuracy = leave_one_out_cross_validation(data, current_set_of_features, k + 2)
-				#print(accuracy)
+			print("--Considering adding the", str(k + 1), "feature")
+			accuracy = leave_one_out(data, current_set_of_features, k + 2)
 
-				if accuracy > best_acc:
-					best_acc = accuracy
-					feature_adding = k
-
-		current_set_of_features.append(feature_adding)
-		print("On level", str(i + 1), "added feature", str(feature_adding), "to current set")		
-'''
-def leave_one_out_cross_validation(data, curr_set, num_lines):
+def leave_one_out(data, num_instances, num_features):
+	accuracy = 0
 	num_correct = 0
-	for i in range(num_lines):
-		n = nearestNeighborAlgorithm(data, i, num_lines, curr_set)
+	for i in range(num_instances):
+		best_so_far = float("inf")
+		best_so_far_loc = float("nan")
+		#print("Looping over rows", str(i + 1))
+		for j in range(num_instances):
+			sum_dist = 0
+			if(i != j):
+				#print("for examplar", str(i + 1), "comparing it to", str(j + 1))
+				for k in range(1, num_features + 1):
+					distance = pow(data[i][k] - data[j][k] , 2)
+					sum_dist = sum_dist + distance
+				sum_dist = math.sqrt(sum_dist)
+				if sum_dist < best_so_far:
+					best_so_far = sum_dist
+					best_so_far_loc = j
+				#print("sum_dist = ", sum_dist)
+		#print("for exemplar", str(i + 1), "I think its nearest neighbor is", str(best_so_far_loc + 1))
 
-		if data[n][0] == data[i][0]:
+		if data[i][0] == data[best_so_far_loc][0]:
+			print("I got exemplar", str(i + 1), "correct")
 			num_correct = num_correct + 1
+	accuracy = num_correct / num_instances
+	#print(accuracy)
+
+	return accuracy
 
 if __name__ == "__main__":
 
 	data = []
 	#features = []
 	num_features = 0
-	num_lines = 0
+	num_instances = 0
+	acc = 0
 	mean = []
 	stand_dev = []
-	filename = "./test.txt"
+	filename = "./test2.txt"
 
 	f=open(filename, "r")
 	fline = f.readline()
+
+
 
 	#print(line.split())
 
@@ -70,21 +62,29 @@ if __name__ == "__main__":
 	f.seek(0)
 
 	for line in f:
-		num_lines = num_lines + 1
+		num_instances = num_instances + 1
 
 	#print(num_lines)
 
 	f.seek(0)
 
-	for i in range(num_lines):
+	for i in range(num_instances):
 		data.append([])
 		for j in f.readline().split():
 			data[i].append(float(j))
 	print(data)
+	print("This dataset has", num_features, "features with", num_instances, "instances")
 
+	
+	acc = leave_one_out(data, num_instances, num_features)
+	print("Running nearest neighbor with all", num_features, "features, using \"leaving-one-out\" evaluation, I get an accuracy of", acc)
+
+	feature_search(data, num_features)
+
+'''
 	for i in range(1, num_features + 1):
 		features = []
 		for row in data:
 			features.append(row[i])
 			print(features)
-			leave_one_out_cross_validation(data, features, num_lines)
+'''
