e3ca0679bc97760188c005a65e097c7399359eb6
Junyoung Kwon <jkwon031@bolt.cs.ucr.edu>
2019-12-10 20:09:37 -0800

    Almost done

--- Project2/p2.py
+++ Project2/p2.py
@@ -1,42 +1,112 @@
 import os
 import math
+import random
+import copy
 
-def feature_search(data, num_features):
+def nearestNeighborForward(current_set_of_features, val, i, j):
+	distance = 0
+	#print(current_set_of_features)
+	temp = copy.deepcopy(current_set_of_features)
+	temp.append(val)
+	for k in range(len(temp)):
+		distance = distance + pow(data[j][temp[k]] - data[i][temp[k]], 2)
+	return distance
+
+def nearestNeighborBackward(current_set_of_features, val, i, j):
+	distance = 0
+	#print(current_set_of_features)
+	temp = copy.deepcopy(current_set_of_features)
+	temp.remove(val)
+	#print(temp)
+	for k in range(len(temp)):
+		distance = distance + pow(data[j][temp[k]] - data[i][temp[k]], 2)
+	return distance
+
+def leave_one_out(data, current_set_of_features, val, flag):
+	num_correct = 0
+	#print(current_set_of_features)
+	for i in range(len(data)):
+		best_so_far = float("inf")
+		best_so_far_loc = float("nan")
+		for j in range(len(data)):
+			if i != j:
+				if flag == 0:
+					distance = nearestNeighborForward(current_set_of_features, val, i, j)
+				elif flag == 1:
+					#print("hi")
+					distance = nearestNeighborBackward(current_set_of_features, val, i, j)
+				#print(distance)
+				if distance < best_so_far:
+					best_so_far = distance
+					best_so_far_loc = j
+		if data[i][0] == data[best_so_far_loc][0]:
+			num_correct = num_correct + 1
+	accuracy = num_correct / len(data)
+	return accuracy					
+
+def forward_selection(data, num_features):
+	flag = 0
 	current_set_of_features = []
 	for i in range(num_features):
 		print("On the", str(i + 1), "th level of the search tree")
+		feature_to_add_at_this_level = 0
+		best_so_far_accuracy = 0
 		for k in range(num_features):
-			print("--Considering adding the", str(k + 1), "feature")
-			accuracy = leave_one_out(data, current_set_of_features, k + 2)
+			if (k + 1) not in current_set_of_features:
+				print("--Considering adding the", str(k + 1), "feature")
+				#print(current_set_of_features)
+				accuracy = leave_one_out(data, current_set_of_features, k + 1, flag)
+				print("accuracy: " + str(accuracy))
+				if accuracy > best_so_far_accuracy:
+					best_so_far_accuracy = accuracy
+					feature_to_add_at_this_level = k + 1
+		current_set_of_features.append(feature_to_add_at_this_level)
+		print("On level", str(i + 1), ", I added feature", str(feature_to_add_at_this_level), "to current set")
+		print(current_set_of_features)		
+		#print(feature_to_add_at_this_level + 1)
 
-def leave_one_out(data, num_instances, num_features):
-	accuracy = 0
-	num_correct = 0
-	for i in range(num_instances):
-		best_so_far = float("inf")
-		best_so_far_loc = float("nan")
-		#print("Looping over rows", str(i + 1))
-		for j in range(num_instances):
-			sum_dist = 0
-			if(i != j):
-				#print("for examplar", str(i + 1), "comparing it to", str(j + 1))
-				for k in range(1, num_features + 1):
-					distance = pow(data[i][k] - data[j][k] , 2)
-					sum_dist = sum_dist + distance
-				sum_dist = math.sqrt(sum_dist)
-				if sum_dist < best_so_far:
-					best_so_far = sum_dist
-					best_so_far_loc = j
-				#print("sum_dist = ", sum_dist)
-		#print("for exemplar", str(i + 1), "I think its nearest neighbor is", str(best_so_far_loc + 1))
+def backward_elimination(data, num_features):
+	flag = 1
+	best_accuracy = 0
+	#best_feature = 0
+	#best_so_far_features = []
+	best_set_of_features = []
+	current_set_of_features = []
+	for i in range(1, num_features + 1):
+		current_set_of_features.append(i)
+	print("This set:", current_set_of_features)
+	for i in range(len(current_set_of_features)):
+		print("On the", str(i + 1), "th level of the search tree")
+		feature_to_remove_at_this_level = 0
+		best_so_far_accuracy = 0
+		best_so_far_features = []
+		#best_feature = 0
+		for k in range(num_features):
+			
+			if (k + 1) in current_set_of_features:
+				print("--Consdering removing the", str(k + 1), "feature")
 
-		if data[i][0] == data[best_so_far_loc][0]:
-			print("I got exemplar", str(i + 1), "correct")
-			num_correct = num_correct + 1
-	accuracy = num_correct / num_instances
-	#print(accuracy)
+				accuracy = leave_one_out(data, current_set_of_features, k + 1, flag)
+				print("accuracy: " + str(accuracy))
+				if accuracy > best_so_far_accuracy:
+					best_so_far_accuracy = accuracy
+					best_so_far_features = current_set_of_features
+					#print(best_so_far_features)
+					feature_to_remove_at_this_level = k + 1
+				if best_so_far_accuracy > best_accuracy:
+					best_set_of_features = best_so_far_features
+					best_accuracy = best_so_far_accuracy
+					print("Best so far:", best_so_far_accuracy)
+					#print(best_set_of_features)
+					#print(best_set_of_features)
+		#best_set_of_features.append(best_feature)
+		current_set_of_features.remove(feature_to_remove_at_this_level)
+		print("On level", str(i + 1), ", I removed feature", str(feature_to_remove_at_this_level), "from current set")
+		print(current_set_of_features)
+		print(best_accuracy, best_set_of_features)
 
-	return accuracy
+
+
 
 if __name__ == "__main__":
 
@@ -47,7 +117,8 @@ if __name__ == "__main__":
 	acc = 0
 	mean = []
 	stand_dev = []
-	filename = "./test2.txt"
+	filename = "./CS170_SMALLtestdata__15.txt"
+	#filename = "./test2.txt"
 
 	f=open(filename, "r")
 	fline = f.readline()
@@ -72,15 +143,15 @@ if __name__ == "__main__":
 		data.append([])
 		for j in f.readline().split():
 			data[i].append(float(j))
-	print(data)
+	#print(data)
 	print("This dataset has", num_features, "features with", num_instances, "instances")
 
 	
-	acc = leave_one_out(data, num_instances, num_features)
-	print("Running nearest neighbor with all", num_features, "features, using \"leaving-one-out\" evaluation, I get an accuracy of", acc)
+	#acc = leave_one_out(data, num_instances, num_features)
+	#print("Running nearest neighbor with all", num_features, "features, using \"leaving-one-out\" evaluation, I get an accuracy of", acc)
 
-	feature_search(data, num_features)
-
+	#forward_selection(data, num_features)
+	backward_elimination(data, num_features)
 '''
 	for i in range(1, num_features + 1):
 		features = []
